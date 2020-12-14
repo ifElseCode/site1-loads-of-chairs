85ea4fde26579824235e7d51389c1e4ea3dc593c
Patricia Pires Dias <ppiresdias@umassd.edu>
2020-12-13 22:50:21 -0500

    installed additional libraries

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
-/* import any required libraries here */
+/* import required libraries */
+
 const express = require('express') // load nodejs framework
+const path = require('path') // utilities for working with file and directory paths
+const cors = require('cors')
+const bodyParser = require('body-parser') // parse request body
 
 // initialize express
 const app = express()
 const port = parseInt(process.env.PORT, 10) || 3000
 
- //serve public files
+//serve public files
 app.use(express.static('public'));
-
+// enable cors
+app.use(cors())
 
 // API routes
 app.get('/', (req, res) => {
